2a3d2deb6a7f655000ff282bfa47d81c5aabb963
luiggi <l.gesuele@studenti.unipi.it>
2018-11-08 18:15:40 +0100

    - class KeyCouple implemented - getSize implemented

--- src/SecureDataContainer.java
+++ src/SecureDataContainer.java
@@ -2,12 +2,13 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
 import java.util.HashMap;
-public class SecureDataContainer<E> implements SecureDataContainerInterface {
 
-    private Map<KeyCouple,Vector> DBUsers;
+public class SecureDataContainer<E> implements SecureDataContainerInterface<E> {
+
+    private Map<KeyCouple,Vector<E>> DBUsers;
 
     public SecureDataContainer(){
-        this.DBUsers = new HashMap<>();
+        DBUsers = new HashMap<>();
     }
 
     @Override
@@ -21,44 +22,70 @@ public class SecureDataContainer<E> implements SecureDataContainerInterface {
     }
 
     @Override
-    public boolean put(String Owner, String passw, Object data) throws NullPointerException,IllegalArgumentException,NoUserException {
+    public boolean put(String Owner, String passw, E data) throws NullPointerException,IllegalArgumentException,NoUserException {
         if(Owner == null || passw == null || data == null) throw new NullPointerException();
         if(Owner.isEmpty() || passw.isEmpty()) throw new IllegalArgumentException();
         KeyCouple user = new KeyCouple(Owner,passw);
         if(DBUsers.containsKey(user)){
-            return DBUsers.get(user).add(data);
+            Vector<E> aux = DBUsers.get(user);
+            return aux.add(data);
         }else{
             throw new NoUserException("Non esiste l'utente richiesto");
         }
     }
 
+
     @Override
-    public Object get(String Owner, String passw, Object data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{
+    public E get(String Owner, String passw, Object data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{
         if(Owner == null || passw == null || data == null) throw new NullPointerException();
         if(Owner.isEmpty() || passw.isEmpty()) throw new IllegalArgumentException();
         KeyCouple user = new KeyCouple(Owner,passw);
         if(DBUsers.containsKey(user)){
             if(DBUsers.get(user).contains(data)){
-                Object aux = data;
-                return aux;
+                return DBUsers.get(user).get(DBUsers.get(user).indexOf(data));
             }else{
                 throw new DataNotFoundException("Non esiste il dato richiesto");
             }
         }else{
-            throw new NoUserException("Non esiste l'utente richiesto");
+            throw new NoUserException("Non esiste il dato richiesto");
         }
     }
 
     @Override
-    public Object remove(String Owner, String passw, Object data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{
-        return null;
+    public E remove(String Owner, String passw, E data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{
+        if(Owner == null || passw == null || data == null) throw new NullPointerException();
+        if(Owner.isEmpty() || passw.isEmpty()) throw new IllegalArgumentException();
+        KeyCouple user = new KeyCouple(Owner,passw);
+        if(DBUsers.containsKey(user)){
+            if(DBUsers.get(user).contains(data)){
+                DBUsers.get(user).remove(DBUsers.get(user).indexOf(data));
+                return data;
+            }else{
+                throw new DataNotFoundException("Non esiste il dato richiesto");
+            }
+        }else{
+            throw new NoUserException("Non esiste il dato richiesto");
+        }
+    }
+    @Override
+    public void copy(String Owner, String passw, E data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{
+        if(Owner == null || passw == null || data == null) throw new NullPointerException();
+        if(Owner.isEmpty() || passw.isEmpty()) throw new IllegalArgumentException();
+        KeyCouple user = new KeyCouple(Owner,passw);
+        if(DBUsers.containsKey(user)){
+            if(DBUsers.get(user).contains(data)){
+                DBUsers.get(user).add(data);
+            }else{
+                throw new DataNotFoundException("Non esiste il dato richiesto");
+            }
+        }else{
+            throw new NoUserException("Non esiste il dato richiesto");
+        }
     }
 
-    @Override
-    public void copy(String Owner, String passw, Object data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{}
 
     @Override
-    public void share(String Owner, String passw, String Other, Object data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{}
+    public void share(String Owner, String passw, String Other, E data) throws NullPointerException,IllegalArgumentException,NoUserException,DataNotFoundException{}
 
     @Override
     public Iterator getIterator(String Owner, String passw) {
